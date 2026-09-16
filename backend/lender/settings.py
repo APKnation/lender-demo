85768b83d1723ded8949f36bd69893e896015e66
APKnation <atanasikafuka@gmail.com>
2026-09-16 11:34:40 +0300

    backend models.py

--- backend/lender/settings.py
+++ backend/lender/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XframeOptionsMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 # --------------------------------------------------------------------------- #
